11c77cfa8453974bd5754429ee40f48ed5ef3908
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-03-10 00:10:27 -0300

    Salvando primeiro usuario no banco de dados.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,22 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ msg: 'UIAHDIUHAIUSD world' }));
+routes.get('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: 'Gabriel Fresatto',
+      email: 'gabrielfresatto@gmail.com',
+      password_hash: '12345678',
+    });
+
+    return res.json(user);
+  } catch (error) {
+    return res.json({
+      error,
+    });
+  }
+});
 
 export default routes;
