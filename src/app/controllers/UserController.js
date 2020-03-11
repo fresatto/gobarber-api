3d53c00ead27f7bf108e2a3fae7b1feadebc2c57
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-03-10 22:41:16 -0300

    Ediçao de usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -25,7 +25,52 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword, password } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // Usuário ta querendo alterar o e-mail
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (userExists) {
+        return res.status(400).json({
+          error: 'User already exsists.',
+        });
+      }
+    }
+
+    // Caso queira alterar a senha, tanto a senha antiga,
+    // quanto a nova, deverão ser fornecidas
+    if (password) {
+      if (!oldPassword) {
+        return res.status(401).json({
+          error: 'oldPassword not provided',
+        });
+      }
+    }
+
+    // Caso o usuário queira inserir uma nova senha
+    // verifica se a senha antiga, bate com a atual
+    // salva no banco de dados
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        error: 'Password does not match',
+      });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      provider,
+      email,
+    });
   }
 }
 
