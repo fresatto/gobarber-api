1fb421e61821b7a9ce80fe6b9e1c35a4466c341e
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-03-10 22:19:29 -0300

    MiddlewareAuth

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // Users
 routes.post('/users', UserController.store);
-
 // Session
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+// Auth Users
+routes.put('/users', UserController.update);
 
 export default routes;
