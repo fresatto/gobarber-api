854a9285f015064907572fcc1b2177ca63eb8c42
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-03-14 15:40:02 -0300

    Template engine para envio de e-mail

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,10 @@
 import nodemailer from 'nodemailer';
 import mailConfig from '../config/mail';
+
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
 class Mail {
   constructor() {
     const { host, port, auth, secure } = mailConfig;
@@ -10,6 +15,26 @@ class Mail {
       auth: auth.user ? auth : null,
       secure,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendEmail(message) {
